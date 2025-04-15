b02f876f760e698474d459b45ec47438f27fd830
AdamJoestar <adamschoolauto@gmail.com>
2025-04-15 22:53:21 +0700

    error handling

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,27 +11,30 @@ class WorldTime {
   WorldTime({required this.location, required this.flag, required this.url});
 
   Future<void> getTime() async {
-    String apiKey = 'lOaZowDZm53aB1/8hjEaIg==1P7fRSlBaLWg9YAU';
-    Response response = await get(
-      Uri.parse('https://api.api-ninjas.com/v1/worldtime?timezone=$url'),
-      headers: {'X-Api-Key': apiKey},
-    );
-    Map data = jsonDecode(response.body);
-    //print(data);
+    try {
+      String apiKey = 'lOaZowDZm53aB1/8hjEaIg==1P7fRSlBaLWg9YAU';
+      Response response = await get(
+        Uri.parse('https://api.api-ninjas.com/v1/worldtime?timezone=$url'),
+        headers: {'X-Api-Key': apiKey},
+      );
+      Map data = jsonDecode(response.body);
+      //print(data);
 
-    // get properties from data
-    String datetime = data['datetime'] ?? 'Unknown datetime';
-    // String day = data['day_of_week'] ?? 'No days'; // Removed unused variable
-    // print(datetime);
-    // print(day);
+      // get properties from data
+      String datetime = data['datetime'] ?? 'Unknown datetime';
+      // String day = data['day_of_week'] ?? 'No days'; // Removed unused variable
+      // print(datetime);
+      // print(day);
 
-    //create DateTime object
-    DateTime now = DateTime.parse(datetime);
+      //create DateTime object
+      DateTime now = DateTime.parse(datetime);
 
-    time = now.toString();
-  
+      time = now.toString();
+    } catch (e) {
+      print('Caught error: $e');
+      time = 'Could not get time data';
+    }
   }
-
 }
 
 WorldTime instance = WorldTime(
